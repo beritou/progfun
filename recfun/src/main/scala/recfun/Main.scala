ed490bd88d238ba5012febd9ed61231a41c6cb90
brenteritou <brent.eritou@boldradius.com>
2016-05-30 16:52:01 -0400

    First test passing

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -15,17 +15,24 @@ object Main {
    */
     def pascal(c: Int, r: Int): Int = {
       def getRow(previousRow:List[Int], previousRowNumber: Int, requestRowNumber: Int): List[Int] = {
+        println("prevRow:" + previousRowNumber + " reqRow: " + requestRowNumber)
         if (requestRowNumber == 0) List(1)
-        else if (previousRow == 0 && requestRowNumber == 1) List(1,1)
+        else if (requestRowNumber == 1 || previousRowNumber == 1) List(1,1)
         else {
           val row = List(1)
-          for (column <- 0 to previousRowNumber - 1)
+          for (column <- 0 to previousRowNumber - 1) {
+            println("List is " + row.length + " elements long, adding one more")
+            println("attempting to access this..." + previousRow)
+            println("column " + column + " and column " + (column +1))
             row :+ previousRow(column) + previousRow(column + 1)
+          }
           if (requestRowNumber == previousRowNumber + 1) row :+ 1
-          else getRow(row :+ 1, previousRowNumber + 1, requestRowNumber)
+          else getRow(row, previousRowNumber + 1, requestRowNumber)
         }
       }
-      getRow(List(1), 0, r)(c)
+    println("getting row " + r + " & column " + c)
+      val theRow = getRow(List(1), 0, r)
+    theRow(c)
   }
   
   /**
