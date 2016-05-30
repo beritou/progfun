cd71fac758b5f2fd433faa0f44307291140af32e
brenteritou <brent.eritou@boldradius.com>
2016-05-30 18:19:34 -0400

    Second test passing

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -12,26 +12,33 @@ object Main {
 
   /**
    * Exercise 1
+    * The numbers at the edge of the triangle are all 1, and each number inside the triangle is the sum
+    * of the two numbers above it. Write a function that computes the elements of Pascal’s triangle by
+    * means of a recursive process.
+    **
+    *Do this exercise by implementing the pascal function in Main.scala, which takes a column c and a row r,
+    *counting from 0 and returns the number at that spot in the triangle.
+    *For example, pascal(0,2)=1,pascal(1,2)=2 and pascal(1,3)=3.
+    *
    */
     def pascal(c: Int, r: Int): Int = {
       def getRow(previousRow:List[Int], previousRowNumber: Int, requestRowNumber: Int): List[Int] = {
         println("prevRow:" + previousRowNumber + " reqRow: " + requestRowNumber)
         if (requestRowNumber == 0) List(1)
-        else if (requestRowNumber == 1 || previousRowNumber == 1) List(1,1)
+        else if (requestRowNumber == 1) List(1,1)
+        else if (previousRowNumber == 0) getRow(List(1,1), 1, requestRowNumber)
         else {
-          val row = List(1)
-          for (column <- 0 to previousRowNumber - 1) {
-            println("List is " + row.length + " elements long, adding one more")
-            println("attempting to access this..." + previousRow)
-            println("column " + column + " and column " + (column +1))
-            row :+ previousRow(column) + previousRow(column + 1)
-          }
-          if (requestRowNumber == previousRowNumber + 1) row :+ 1
-          else getRow(row, previousRowNumber + 1, requestRowNumber)
+          val rowWithIntermediateNumbers =
+            for (column <- 0 to previousRowNumber - 1)
+              yield previousRow(column).toInt + previousRow(column + 1).toInt
+
+          if (requestRowNumber == previousRowNumber + 1) List(1) ++ rowWithIntermediateNumbers.toList ++ List(1)
+          else getRow(List(1) ++ rowWithIntermediateNumbers.toList, previousRowNumber + 1, requestRowNumber)
         }
       }
     println("getting row " + r + " & column " + c)
-      val theRow = getRow(List(1), 0, r)
+    val theRow = getRow(List(1), 0, r)
+    println(theRow)
     theRow(c)
   }
   
